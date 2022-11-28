06124319a74d23aebe57412f338bb323686ac06d
tugi-ngunjiri <blueqwinn@gmail.com>
2022-11-28 21:02:50 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  
+  return year
